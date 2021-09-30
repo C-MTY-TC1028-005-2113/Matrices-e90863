9be0d0130d770d174a0665ba5a2d67bd98a1ee93
roque63 <roque@itesm.mx>
2021-09-30 18:10:57 -0500

    Update exercise.py

--- assignments/15SumaColumnas/src/exercise.py
+++ assignments/15SumaColumnas/src/exercise.py
@@ -2,26 +2,12 @@
 # leer elemento por elemento - 
 def leer_matriz (renglon, columna):
     matriz = []
-    for r in range(renglon):
-        row = []
-        for c in range(columna):
-            valor = int(input())
-            row.append(valor)
-        matriz.append(row)  
+    
     return matriz
 
 def suma_columnas (matriz):
     lista_suma = []
-    # Recorrido por columnas
-    for c in range(len(matriz[0])):
-        acum = 0
-        for r in range(len(matriz)):
-            # sacar el elemento  matriz[r][c]  - r, c
-            acum = acum + matriz[r][c]
-        
-        # añadir a la lista_suma append
-        lista_suma.append(acum)
-        
+    
     return lista_suma
 
 
